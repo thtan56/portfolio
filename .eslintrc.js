d2e77189ce3af6ac52ad5d0cb9c1808df45660bc
tan thian huat <t.h.tan@dunelm.org.uk>
2019-12-09 17:50:34 +0800

    Delete .eslintrc.js

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: ["plugin:vue/recommended"],
-  rules: {
-    'no-console': 'off',
-    "vue/order-in-components": 'off',
-    "vue/require-default-prop": 'off',
-    'vue/singleline-html-element-content-newline': 'off'
-  }
-};
